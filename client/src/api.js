450526b2d1a2a56ee3fff074676ad083c5d12cdf
JadePablo <98762929+JadePablo@users.noreply.github.com>
2023-06-26 02:14:21 -0400

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = "https://diario-sykr.onrender.com"
+const URL = process.env.BACKEND_URL
 
 // Function to handle login success and verify the token
 async function handleLoginSuccess(token) {
